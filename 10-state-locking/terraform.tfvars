70d0848d2de67c5ace364529eb0e5a796f557fc9
shanu-485 <mrshanu485@gmail.com>
2026-08-07 20:09:19 +0000

    Day 10: Terraform DynamoDB state locking

---
+++ 10-state-locking/terraform.tfvars
@@ -0,0 +1,2 @@
+bucket_name = "shanu-lock-demo-20260808-008"
+region       = "ap-south-1"
---
+++ 10-state-locking/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "bucket_name" {
+  type = string
+}
