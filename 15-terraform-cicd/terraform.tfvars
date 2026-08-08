a3e3a1701cf1167de229827f8ae0fbe67176e1d6
shanu-485 <mrshanu485@gmail.com>
2026-08-08 18:50:12 +0000

    Day 15: Terraform CI with GitHub Actions

---
+++ 15-terraform-cicd/terraform.tfvars
@@ -0,0 +1,2 @@
+region      = "ap-south-1"
+bucket_name = "shanu-cicd-demo-20260808-010"
---
+++ 15-terraform-cicd/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
