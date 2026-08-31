05712846acb42431caeb8d5d9caf9312f427a1b4
shanu-485 <mrshanu485@gmail.com>
2026-08-31 01:16:43 +0000

    Day 27: Dynamic AMI data source

---
+++ 27-data-sources-ami/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Find the latest Amazon Linux 2023 AMI automatically
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "ami_id" {
+  value = data.aws_ami.amazon_linux.id
+}
+
+output "ami_name" {
+  value = data.aws_ami.amazon_linux.name
+}
