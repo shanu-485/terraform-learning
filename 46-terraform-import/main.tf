cdacac1db72f681485b38939a7fbc256804cb24a
shanu-485 <mrshanu485@gmail.com>
2026-09-05 03:06:08 +0000

    Day 46: Terraform import

---
+++ 46-terraform-import/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "imported" {
+  name        = "day32-web-sg-2"
+  description = "Day 32 count example"
+  vpc_id      = "vpc-01abde828c92c4ca9"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "day32-web-sg-2"
+    Project = "Terraform Learning"
+  }
+}
