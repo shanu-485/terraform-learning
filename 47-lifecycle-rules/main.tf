cfdbf58f591ad5165517c4e5417dea61b2706baa
shanu-485 <mrshanu485@gmail.com>
2026-09-08 17:58:44 +0000

    Day 47: Terraform lifecycle rules

---
+++ 47-lifecycle-rules/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "web" {
+  name        = "day47-lifecycle-sg-v2"
+  description = "Day 47 Terraform lifecycle demo"
+  vpc_id      = "vpc-01abde828c92c4ca9"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name    = "day47-lifecycle-sg"
+    Project = "Terraform Learning"
+    Day     = "47"
+  }
+}
+
